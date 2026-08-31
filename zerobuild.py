cabc6794300961c90e19bd4bb0feab4a6f0cc2c7
haseeb <haseeeb8j@gmail.com>
2026-08-31 22:41:20 +0500

    removed: assembler; updated: merged Instr to Runtime

--- zerobuild.py
+++ zerobuild.py
@@ -20,15 +20,6 @@ build.arguments = Flags.gcc.Wall, Flags.gcc.Wextra, Flags.gcc.g
 build.export_compile_commands = True
 
 
-# Cute Instructions
-
-CuteInstr = StaticLibrary()
-CuteInstr.source = Source(
-	Path("Instr") / "image.c"
-)
-CuteInstr.headers.public = Path("Instr") / "include"
-CuteInstr.arguments = C_STANDARD
-
 # Cute Engine
 
 CuteRuntime = StaticLibrary()
@@ -41,14 +32,15 @@ CuteRuntime.source = Source(
 	src / "core" / "core.c",
 	src / "core" / "exec.c",
 	src / "core" / "context.c",
+
+	src / "image" / "image.c",
+
 	src / "objects" / "manager.c",
 	src / "container" / "container.c",
 	src / "utils" / "utils.c",
 
 	src / "lib" / "buffer.c",
 )
-
-CuteRuntime.link(CuteInstr)
 
 if DEBUG:
 	CuteRuntime.arguments = Flags.Macro("CT_CONF_DEBUG"), C_STANDARD
@@ -61,31 +53,3 @@ else:
 cute = Executable()
 cute.source = Source("main/runtime.c")
 cute.link(CuteRuntime)
-
-
-# Cute Assembler
-
-CuteAsm = StaticLibrary()
-CuteAsm.compiler = "g++"
-
-src = Path("Assembler")
-
-CuteAsm.headers.private = src
-CuteAsm.headers.public = src / "include"
-CuteAsm.source = Source(
-	src / "tokenizer" / "tokenizer.cpp",
-	src / "tokenizer" / "stream.cpp",
-	src / "codegen" / "codegen.cpp",
-	src / "assembler" / "assembler.cpp"
-)
-
-CuteAsm.arguments = CPP_STANDARD
-CuteAsm.link(CuteInstr)
-
-
-# cuteasm binary
-
-cuteasm = Executable()
-cuteasm.compiler = CuteAsm.compiler
-cuteasm.source = Source("main/assembler.cpp")
-cuteasm.link(CuteAsm)
